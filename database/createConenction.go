3b108df0b74ddde246429a72faa2c386e4b7ff68
VirajParab <parabveer01@gmail.com>
2016-04-20 13:28:34 +0530

    added schema search path

--- database/createConenction.go
+++ database/createConenction.go
@@ -10,11 +10,12 @@ const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "postgres"
 	DB_NAME     = "postgres"
+	DB_SCHEMA   = "todoMaker"
 )
 
 func CreateConnection() *sql.DB{
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable SEARCH_PATH=%s ",
+		DB_USER, DB_PASSWORD, DB_NAME, DB_SCHEMA)
 	db, err := sql.Open("postgres", dbinfo)
 	errorHandler.DatabaseErrorHandler(err)
 	return db
